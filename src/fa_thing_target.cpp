27ed95bea5c9c1b0655f3ca5790949d3634e1ec1
Stefan Schmidt <snst@meek.de>
2023-01-16 01:02:32 +0100

    restructure menu

--- src/fa_thing_target.cpp
+++ src/fa_thing_target.cpp
@@ -42,7 +42,6 @@ void thingSetup()
   thing["Settings"] << [](pson &in)
   {
     bool isEmpty = in.is_empty();
-
     valUpdate(in, "1.1 Controller mode", settings.mode, isEmpty);
     valUpdate(in, "1.2 Controller intervall sec", settings.controller_interval_sec, isEmpty);
     valUpdate(in, "1.3 Sensor read intervall sec", settings.temp_sensor_read_interval_sec, isEmpty);
@@ -50,50 +49,46 @@ void thingSetup()
     valUpdate(in, "1.5 Enable sensor calibration temp", settings.use_calibration_temp, isEmpty);
     valUpdate(in, "1.6 Enable sensor calibration humidity", settings.use_calibration_humidity, isEmpty);
     valUpdate(in, "1.7 Log mask", settings.log_mask, isEmpty);
-
     valUpdate(in, "2.1 Sniffing enabled", settings.sniff.enabled, isEmpty);
     valUpdate(in, "2.2 Sniffing fan level", settings.sniff.fan_level, isEmpty);
     valUpdate(in, "2.3 Sniffing duration sec", settings.sniff.duration_sec, isEmpty);
     valUpdate(in, "2.4 Sniffing intervall sec", settings.sniff.interval_sec, isEmpty);
-
-    valUpdate(in, "3.1 Manual fan level fresh", settings.manual.level_fan_fresh, isEmpty);
-    valUpdate(in, "3.2 Manual fan level exhaust", settings.manual.level_fan_exhaust, isEmpty);
-    valUpdate(in, "3.3 Manual fan level frost", settings.manual.level_fan_frost, isEmpty);
-    valUpdate(in, "3.4 Manual frost flap open", settings.manual.open_flap_frost, isEmpty);
-
-    valUpdate(in, "4.1 Fan level main min", settings.ctrl.fan_level_min, isEmpty);
-    valUpdate(in, "4.2 Fan level main max", settings.ctrl.fan_level_max, isEmpty);
-    valUpdate(in, "4.3 Fan level frost min", settings.ctrl.fan_frost_level_min, isEmpty);
-    valUpdate(in, "4.4 Fan level frost max", settings.ctrl.fan_frost_level_max, isEmpty);
-
+    valUpdate(in, "3.1 Fan level main min", settings.ctrl.fan_level_min, isEmpty);
+    valUpdate(in, "3.2 Fan level main max", settings.ctrl.fan_level_max, isEmpty);
+    valUpdate(in, "3.3 Fan level frost min", settings.ctrl.fan_frost_level_min, isEmpty);
+    valUpdate(in, "3.4 Fan level frost max", settings.ctrl.fan_frost_level_max, isEmpty);
     force_update = !isEmpty;
   };
 
-  thing["Controller"] << [](pson &in)
+  thing["Manual"] << [](pson &in)
   {
     bool isEmpty = in.is_empty();
-    valUpdate(in, "1.1 Enable general humidity conditions for fan", settings.ctrl.humidity_fan_ctrl.enabled, isEmpty);
-    valUpdate(in, "1.2 Start fan if abs humidity delta greater than g/m³", settings.ctrl.humidity_fan_ctrl.abs_min_start, isEmpty);
-    valUpdate(in, "1.3 Stop fan if abs humidity delta less than g/m³", settings.ctrl.humidity_fan_ctrl.abs_min_stop, isEmpty);
-    valUpdate(in, "1.4 Stop fan if rel humidity less than %", settings.ctrl.humidity_fan_ctrl.rel_min_start, isEmpty);
-    valUpdate(in, "2.1 Enabled temp-fan curve", settings.ctrl.temp_fan_curve.enabled, isEmpty);
-    valUpdate(in, "2.2 Used temp-fan curve points", settings.ctrl.temp_fan_curve.enabled_points, isEmpty);
-    valUpdate(in, "3.1 Enabled humidity-fan curve", settings.ctrl.humidity_fan_curve.enabled, isEmpty);
-    valUpdate(in, "3.2 Used humidity-fan curve points", settings.ctrl.humidity_fan_curve.enabled_points, isEmpty);
-    valUpdate(in, "4.1 Enable frost flap", settings.ctrl.frost_flap_ctrl.enabled, isEmpty);
-    valUpdate(in, "4.2 Open flap if temp is lesser than °C", settings.ctrl.frost_flap_ctrl.temp_min_open, isEmpty);
-    valUpdate(in, "4.3 Close flap if temp is greater than °C", settings.ctrl.frost_flap_ctrl.temp_min_close, isEmpty);
-    valUpdate(in, "5.1 Enabled frost-fan curve", settings.ctrl.frost_fan_curve.enabled, isEmpty);
-    valUpdate(in, "5.2 Used frost-fan curve points", settings.ctrl.frost_fan_curve.enabled_points, isEmpty);
+    valUpdate(in, "1 Manual fan level fresh", settings.manual.level_fan_fresh, isEmpty);
+    valUpdate(in, "2 Manual fan level exhaust", settings.manual.level_fan_exhaust, isEmpty);
+    valUpdate(in, "3 Manual fan level frost", settings.manual.level_fan_frost, isEmpty);
+    valUpdate(in, "4 Manual frost flap open", settings.manual.open_flap_frost, isEmpty);
+    force_update = !isEmpty;
+  };
+
+  thing["Controller general"] << [](pson &in)
+  {
+    bool isEmpty = in.is_empty();
+    valUpdate(in, "1 Enable general humidity conditions for fan", settings.ctrl.humidity_fan_ctrl.enabled, isEmpty);
+    valUpdate(in, "2 Start fan if abs humidity delta greater than g/m³", settings.ctrl.humidity_fan_ctrl.abs_min_start, isEmpty);
+    valUpdate(in, "3 Stop fan if abs humidity delta less than g/m³", settings.ctrl.humidity_fan_ctrl.abs_min_stop, isEmpty);
+    valUpdate(in, "4 Stop fan if rel humidity less than %", settings.ctrl.humidity_fan_ctrl.rel_min_start, isEmpty);
     settingsSanitize();
+    force_update = !isEmpty;
+  };
 
-    protoson::pson_array &array_temp_temp = in["2.3 Temp less than °C"];
-    protoson::pson_array &array_temp_level = in["2.4 Fan level"];
-    protoson::pson_array &array_hum_hum = in["3.3 Abs humidity delta less than g/m³"];
-    protoson::pson_array &array_hum_level = in["3.4 Fan level"];
-    protoson::pson_array &array_frost_temp_temp = in["5.3 Temp less than °C"];
-    protoson::pson_array &array_frost_temp_level = in["5.4 Fan level"];
-
+  thing["Controller temperature"] << [](pson &in)
+  {
+    bool isEmpty = in.is_empty();
+    valUpdate(in, "1 Enabled temp-fan curve", settings.ctrl.temp_fan_curve.enabled, isEmpty);
+    valUpdate(in, "2 Used temp-fan curve points", settings.ctrl.temp_fan_curve.enabled_points, isEmpty);
+    settingsSanitize();
+    protoson::pson_array &array_temp_temp = in["3 Temp less than °C"];
+    protoson::pson_array &array_temp_level = in["4 Fan level"];
     for (uint8_t i = 0U; i < settings.ctrl.temp_fan_curve.enabled_points; i++)
     {
       if (isEmpty)
@@ -107,6 +102,17 @@ void thingSetup()
         settings.ctrl.temp_fan_curve.item[i].level = *array_temp_level[i];
       }
     }
+    force_update = !isEmpty;
+  };
+
+  thing["Controller humidity"] << [](pson &in)
+  {
+    bool isEmpty = in.is_empty();
+    valUpdate(in, "1 Enabled humidity-fan curve", settings.ctrl.humidity_fan_curve.enabled, isEmpty);
+    valUpdate(in, "2 Used humidity-fan curve points", settings.ctrl.humidity_fan_curve.enabled_points, isEmpty);
+    settingsSanitize();
+    protoson::pson_array &array_hum_hum = in["3 Abs humidity delta less than g/m³"];
+    protoson::pson_array &array_hum_level = in["4 Fan level"];
     for (uint8_t i = 0U; i < settings.ctrl.humidity_fan_curve.enabled_points; i++)
     {
       if (isEmpty)
@@ -120,6 +126,20 @@ void thingSetup()
         settings.ctrl.humidity_fan_curve.item[i].level = *array_hum_level[i];
       }
     }
+    force_update = !isEmpty;
+  };
+
+  thing["Controller frost"] << [](pson &in)
+  {
+    bool isEmpty = in.is_empty();
+    valUpdate(in, "1 Enable frost flap", settings.ctrl.frost_flap_ctrl.enabled, isEmpty);
+    valUpdate(in, "2 Open flap if temp is lesser than °C", settings.ctrl.frost_flap_ctrl.temp_min_open, isEmpty);
+    valUpdate(in, "3 Close flap if temp is greater than °C", settings.ctrl.frost_flap_ctrl.temp_min_close, isEmpty);
+    valUpdate(in, "4 Enabled frost-fan curve", settings.ctrl.frost_fan_curve.enabled, isEmpty);
+    valUpdate(in, "5 Used frost-fan curve points", settings.ctrl.frost_fan_curve.enabled_points, isEmpty);
+    settingsSanitize();
+    protoson::pson_array &array_frost_temp_temp = in["6 Temp less than °C"];
+    protoson::pson_array &array_frost_temp_level = in["7 Fan level"];
     for (uint8_t i = 0U; i < settings.ctrl.frost_fan_curve.enabled_points; i++)
     {
       if (isEmpty)
@@ -133,7 +153,6 @@ void thingSetup()
         settings.ctrl.frost_fan_curve.item[i].level = *array_frost_temp_temp[i];
       }
     }
-
     force_update = !isEmpty;
   };
 
@@ -238,18 +257,20 @@ void thingSetup()
     out["simulate"] = fa_ota.simulate;
   };
 
-  ADD_CMD("a Settings save", settingsWrite)
-  ADD_CMD("b Settings load", settingsLoad)
-  ADD_CMD("c Settings clear", settingsClear)
-  ADD_CMD("d Calibration save", calibrationWrite)
-  ADD_CMD("e Calibration load", calibrationLoad)
-  ADD_CMD("f Calibration clear", calibrationClear)
-  ADD_CMD("g Calibrate temp min", sensorsCalibrateTempLow)
-  ADD_CMD("h Calibrate temp max", sensorsCalibrateTempHigh)
-  ADD_CMD("i Calibrate humidity min", sensorsCalibrateHumidityLow)
-  ADD_CMD("j Calibrate humidity max", sensorsCalibrateHumidityHigh)
-  ADD_CMD("k Scan sensors", sensorsScan)
-  ADD_CMD("l Reboot", ESP.restart)
+
+  ADD_CMD("a Start sniffing", controllerStartSniff)
+  ADD_CMD("b Settings save", settingsWrite)
+  ADD_CMD("c Settings load", settingsLoad)
+  ADD_CMD("d Settings clear", settingsClear)
+  ADD_CMD("e Calibration save", calibrationWrite)
+  ADD_CMD("f Calibration load", calibrationLoad)
+  ADD_CMD("g Calibration clear", calibrationClear)
+  ADD_CMD("h Calibrate temp min", sensorsCalibrateTempLow)
+  ADD_CMD("i Calibrate temp max", sensorsCalibrateTempHigh)
+  ADD_CMD("j Calibrate humidity min", sensorsCalibrateHumidityLow)
+  ADD_CMD("k Calibrate humidity max", sensorsCalibrateHumidityHigh)
+  ADD_CMD("l Scan sensors", sensorsScan)
+  ADD_CMD("m Reboot", ESP.restart)
   ADD_CMD("n Start OTA simulation", otaStartSim)
   ADD_CMD("o Start OTA update", otaStart)
   ADD_CMD("p Abort OTA", otaAbort)
